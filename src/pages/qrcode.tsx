1242e7ae234b9665bb4009b31050af20a294a3ad
GitYVR <yvrgithub@proton.me>
2023-02-09 16:37:22 -0800

    qrcode

--- src/pages/qrcode.tsx
+++ src/pages/qrcode.tsx
@@ -1,4 +1,14 @@
-import { Grid, Typography, CircularProgress, Alert } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  CircularProgress,
+  Alert,
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
@@ -9,8 +19,11 @@ import {
   USDT_ADDRESS,
   DAI_ADDRESS,
 } from "../common/constants";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { useSnackbar } from "notistack";
 
 function QRCodePage() {
+  const { enqueueSnackbar } = useSnackbar();
   const [usdPerMatic, setUsdPerMatic] = useState<null | number>(null);
 
   // 1 to 6
@@ -34,6 +47,34 @@ function QRCodePage() {
         <Grid item xs={1} md={3} />
         <Grid item xs={10} md={6}>
           <Alert severity="info">Make sure you're on the Polygon Network</Alert>
+          <Typography variant="h4">Polygon Address</Typography>
+          <QRCode value={`ethereum:${PAYMENT_ADDRESS}`} />
+          <br />
+          <FormControl sx={{ m: 1, width: "50ch" }} variant="standard">
+            <InputLabel htmlFor="standard-adornment-password">
+              Payment Address
+            </InputLabel>
+            <Input
+              value={PAYMENT_ADDRESS}
+              fullWidth
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => {
+                      navigator.clipboard.writeText(PAYMENT_ADDRESS);
+                      enqueueSnackbar("Copied address", { variant: "info" });
+                    }}
+                  >
+                    <ContentCopyIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <Typography marginTop={5} variant="h4">
+            Direct Payments
+          </Typography>
           <Typography variant="h5">Matic</Typography>
           {usdPerMatic === null ? (
             <CircularProgress />
